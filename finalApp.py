3906d86f70562fb9b384349decbfc05d1901f9f6
Ab Basit Rafi Syed <Syedab@msu.edu>
2024-11-30 22:14:18 -0500

    Update finalApp.py

--- finalApp.py
+++ finalApp.py
@@ -188,7 +188,8 @@ def main():
 
     # User Role Selection
     user_role = st.sidebar.selectbox("Who are you?", ["Select User","Public User", "Official User"])
-    
+
+user_role == "Select User"
         select_user_section()
     if user_role == "Public User":
         public_user_section()
