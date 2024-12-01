928e7dff20f7e645a2a0484c521819ba975d7d77
Ab Basit Rafi Syed <Syedab@msu.edu>
2024-11-30 21:55:26 -0500

    Update finalApp.py

--- finalApp.py
+++ finalApp.py
@@ -116,9 +116,6 @@ def public_user_section():
     st.subheader("Future Collisions Calendar")
     st.write("Future collision probabilities will appear here (placeholder).")
     
-def public_user_prediction():
-    st.subheader("Collision Prediction Calendar")
-    st.write("Explore potential collision dates (fake data).")
     selected_date = st.date_input("Choose a Date")
 
     # Fake collision data
